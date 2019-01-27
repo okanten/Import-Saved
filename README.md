0fbc472cbbd91e4e4ac9b7d8962d0272789a035b
O.K <okgit@protonmail.com>
2019-01-27 07:51:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Import-Saved
+Imports saved posts on reddit to new account
+
+It's a bit slow, but it works. 
+
+Inspired by [this](https://github.com/dharmanshu24/Import-Subreddits)
---
+++ importsaved.py
@@ -0,0 +1,48 @@
+import praw
+import getpass
+from praw.models import Submission
+from praw.models import Comment
+
+def get_instance():
+    username = input("username=")
+    password = getpass.getpass()
+    client_id = input("client_id=")
+    client_secret = input("client_secret=")
+    return praw.Reddit(client_id=client_id, client_secret=client_secret, password=password, user_agent=username, username=username)
+
+try:
+    order = input('Keep correct order of saved items? [Y/n]')
+except SyntaxError:
+    order = None
+
+print("Login to old Account")
+old_account = get_instance()
+
+print("Login to new Account")
+new_account = get_instance()
+
+print('Getting list of saved items from old Account')
+
+saved = old_account.user.me().saved(limit=None)
+
+reversed_list = []
+
+for item in saved:
+    if isinstance(item, Submission):
+        reversed_list.append('Submission {}'.format(item))
+    else:
+        reversed_list.append('Comment {}'.format(item))
+        print('Comment {}'.format(item))
+
+if order == 'Y' or order == None:
+    reversed_list.reverse()
+
+for item in reversed_list:
+    if item.startswith('Comment'):
+        item_id = item.replace("Comment ", "")
+        print('Saving comment {}'.format(item_id))
+        new_account.comment(id=item_id).save()
+    else:
+        item_id = item.replace("Submission ", "")
+        print('Saving submission {}'.format(item_id))
+        new_account.submission(id=id2).save()
